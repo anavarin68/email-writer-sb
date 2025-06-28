7a535a8c184974cc403e9642d234e69de2154c29
Nitish <kumarpateln7@gmail.com>
2025-06-28 12:48:40 +0530

    Crafted the prompt request for API

--- src/main/java/com/email/writer/EmailGeneratorService.java
+++ src/main/java/com/email/writer/EmailGeneratorService.java
@@ -2,14 +2,24 @@ package com.email.writer;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class EmailGeneratorService {
 
     public String generateEmailReply(EmailRequest emailRequest){
         //Build the prompt
-        String prompt =  BuildPrompt(emailRequest);
+        String prompt =  buildPrompt(emailRequest);
 
         //Craft the request
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[]{
+                        Map.of("parts", new Object[]{
+                            Map.of("text", prompt)
+                })
+                }
+        );
+
         //Do request and get reponse
         //Return Response
 
@@ -17,7 +27,7 @@ public class EmailGeneratorService {
 
     private String buildPrompt(EmailRequest emailRequest) {
         StringBuilder prompt = new StringBuilder();
-        prompt.append("Generate a professional email reply for the following content. Please don't generate a subject line");
+        prompt.append("Generate a professional email reply for the following content. Please don't generate a subject line ");
         if(emailRequest.getTone() != null && emailRequest.getTone().isEmpty()){
             prompt.append("Use a").append(emailRequest.getTone()).append(" tone.");
         }
