40cababcda9adb6cd3f257316c45de5115027e1f
Nitish <kumarpateln7@gmail.com>
2025-08-25 20:12:22 +0530

    Updated

--- src/main/java/com/email/writer/EmailGeneratorService.java
+++ src/main/java/com/email/writer/EmailGeneratorService.java
@@ -1,6 +1,5 @@
 package com.email.writer;
 
-import com.email.writer.EmailRequest;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
